56fad60c3d3686a8c090435322baaa03a8b10a7c
msamancioglu <mustafa@devtagon.com>
2021-05-14 10:19:08 +0200

    ok

--- index.js
+++ index.js
@@ -88,6 +88,8 @@ app.post('/login', (req, res) => {
 });
 
 
+// ekleme
+
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
